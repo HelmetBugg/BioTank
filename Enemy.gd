6083bd9ac8f8cc46559ad49a805fe61b71cd40a0
malceore <malceore@gmail.com>
2022-12-16 19:51:50 -0500

    moved enemies, made enmies respawn at their points.

--- Enemy.gd
+++ Enemy.gd
@@ -1,28 +1,28 @@
 extends KinematicBody
  
 const MOVE_SPEED = 3
- 
 var player = null
 var dead = false
+var startingPosition
  
 func _ready():	
 	add_to_group("enemies")
- 
+	startingPosition = transform.origin
+	
 func _physics_process(delta):
 	if dead:
 		return
 	if player == null:
 		return
- 
 	var vec_to_player = player.translation - translation
 	vec_to_player = vec_to_player.normalized()
- 
 	move_and_collide(vec_to_player * MOVE_SPEED * delta)
  
 func kill():
-	dead = true
-	$CollisionShape.disabled = true
-	queue_free()
+	#dead = true
+	#$CollisionShape.disabled = true
+	#queue_free()
+	transform.origin = startingPosition
  
 func set_player(p):
 	player = p
