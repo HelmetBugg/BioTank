647dfcdacfd968553642619cfc6a631edf21a062
TheCrusty <zezoku@yahoo.com>
2023-03-02 20:04:11 -0500

    added oninventoryexit functionality for enemyInventory

--- EnemyInventory.gd
+++ EnemyInventory.gd
@@ -8,3 +8,6 @@ func _on_InventoryArea_body_entered(body):
 	print(str('Body entered: ', body.get_name()))
 	body.within_inventory  = true
 	
+	
+func _on_InventoryArea_body_exited(body):
+	body.within_inventory  = false
