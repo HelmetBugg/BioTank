3324a73d96530adc5e9c8808528816728a994428
HelmetBug <Powpengo@gmail.com>
2023-03-16 20:09:38 -0400

    added files

---
+++ Item.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var original_position = Vector2()
+var dragging = false
+var within_inventory = false
+var currentLocation = null
+
+func _process(delta):
+	if dragging:
+		self.global_position = get_global_mouse_position()
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			original_position = self.global_position
+			dragging = true
+		elif event.button_index == BUTTON_LEFT and !event.pressed:
+			dragging = false
+			if !within_inventory: 
+				# Reset position.
+				self.global_position = original_position
+			else:
+				get_parent().remove_child(self)
+				currentLocation.add_child(self)
+				print("Parent got")
+				self.global_position = get_global_mouse_position()
